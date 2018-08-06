23f573581d24d0af02db9f696030d0699d7b9eac
CORPNET\gisilk <gihan.siriwardhana@ifsworld.com>
2018-08-06 23:16:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gisilk/techmeetup/ExplorerFragment.java
@@ -0,0 +1,19 @@
+package com.example.gisilk.techmeetup;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ExplorerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_explorer,container,false);
+    }
+}
