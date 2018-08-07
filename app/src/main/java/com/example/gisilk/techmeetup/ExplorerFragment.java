c62edaffb3e8d090a1530134e0a0dbeac3534c4c
CORPNET\gisilk <gihan.siriwardhana@ifsworld.com>
2018-08-07 07:37:45 +0530

    Exporer fragment and plus add button button modified

--- app/src/main/java/com/example/gisilk/techmeetup/ExplorerFragment.java
+++ app/src/main/java/com/example/gisilk/techmeetup/ExplorerFragment.java
@@ -1,5 +1,6 @@
 package com.example.gisilk.techmeetup;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +8,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ExplorerFragment extends Fragment {
 
+    private Spinner spinner;
+    private static final String[] tags = {"Select a tag","Android","Angular","Java","Ionic","Cordova","C#","Blockchain","Azure Machine Learning"};
+
+    @SuppressLint("ResourceType")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_explorer,container,false);
+            View v =inflater.inflate(R.layout.fragment_explorer,container,false);
+
+
+        spinner = v.findViewById(R.id.dropdownTags);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),R.layout.support_simple_spinner_dropdown_item, tags);
+
+
+        spinner.setAdapter(adapter);
+
+        return v;
+
     }
 }
